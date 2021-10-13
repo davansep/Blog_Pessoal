4c2682a0fd8214dda45f65e8167f090ce0f63b55
davansep <priscila.davanse@gmail.com>
2021-10-13 12:24:34 -0300

    [CREATE PACKAGE SERVICE] 2021.10.13

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -9,20 +9,25 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 /**
- * Classe espelho da tabela usuario no banco db_blogpessoal.
- * 
- * @author Turma34
+ * @author Priscila
  * @since 1.0
- *
  */
-
 @Entity
 public class Usuario {
 
-	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idUsuario;
-	private @NotBlank String nome;
-	private @Email String email;
-	private @NotBlank @Size(min = 5, max = 100) String senha;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUsuario;
+
+	@NotBlank
+	private String nome;
+
+	@Email
+	private String email;
+
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String senha;
 
 	public Long getIdUsuario() {
 		return idUsuario;

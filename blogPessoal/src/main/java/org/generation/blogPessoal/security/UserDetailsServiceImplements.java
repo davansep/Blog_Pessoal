dd2df16626cce98d966ce06870075cbeec391942
davansep <priscila.davanse@gmail.com>
2021-10-13 12:23:59 -0300

    [CREATE PACKAGE SECURITY] 2021.10.13

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/security/UserDetailsServiceImplements.java
@@ -0,0 +1,29 @@
+package org.generation.blogPessoal.security;
+
+import java.util.Optional;
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImplements implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		Optional<Usuario> objetoOptional = repository.findByEmail(username);
+
+		if (objetoOptional.isPresent()) {
+			return new UserDetailsImplements(objetoOptional.get());
+		} else {
+			throw new UsernameNotFoundException(username + " Não existe!");
+		}
+	}
+}
